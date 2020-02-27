d55a8611b9a5891c5590d7cde1707e8e5a5d39cc
Vladimir Mazheru <v_mazheru@yahoo.com>
2020-02-27 08:17:39 -0500

    Rename test

--- mytools.decorator.retry/src/test/java/mytools/function/decorator/retry/RetryDecoratorTest.java
+++ mytools.decorator.retry/src/test/java/mytools/function/decorator/retry/RetryDecoratorTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import mytools.function.object.Counter;
 
-public class RetryDecoratorsTest {
+public class RetryDecoratorTest {
 
     private final LinearRetryPolicy p = new LinearRetryPolicy(3, 10);
     private final Runnable runnableWithRuntimeException = () -> {
